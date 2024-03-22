b25db16b67f2b5456e101171d65a3b5a6cb396b9
Garett-Smith <142698158+Garett-Smith@users.noreply.github.com>
2024-03-22 16:29:52 +0000

    Fri, Mar 22, 2024, 9:29 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -1,10 +1,24 @@
-rulesBtn = document.getElementById('rules-btn')
-rules = document.getElementById('rules')
-closeBtn = document.getElementById('close-btn')
-canvas = document.getElementById('canvas')
-ctx = canvas.getContext('2d')
+rules = document.getElementById('rules-btn')
+hiderules = document.getElementById('close-btn')
+toggle = document.querySelector('.rules')
+score = 0
 
-//create ball properties
+
+// Rules open and close event handlers
+rules.addEventListener('click', () => {
+    toggle.classList.add('show')
+})
+
+hiderules.addEventListener('click', () => {
+    toggle.classList.remove('show')
+})
+
+
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+
+// Create ball properties
 ball = {
     x: canvas.width / 2,
     y: canvas.height / 2,
@@ -13,32 +27,51 @@ ball = {
     dx: 4,
     dy: -4,
 }
-//create paddle properties
-paddle = {
-    x: canvas.width / 2 - 40,
-    y: canvas.height - 20,
-    w: 80,
-    h: 10;
-    speed: 8,
-    dx: 0,
-}
-//draw ball on canvas
-function drawBall(){
+
+
+// Draw ball on canvas
+function drawBall() {
     ctx.beginPath()
-    ctx.arc(ball.x, ball.y, 50, 0, Math.PI * 2)
-    ctx.fillStyle = '#009599'
+    ctx.arc(ball.x,ball.y,ball.size,0,Math.PI * 2)
+    ctx.fillStyle = '#0095dd'
     ctx.fill()
     ctx.closePath()
 }
 
-    drawBall()
 
-rulesBtn.addEventListener('click', () => {
-    rules.classList.add('show')
-})
-
-closeBtn.addEventListener('click', ()
-=>{
-    rules.classList.remove('show')
+// Create paddle properties
+paddle = {
+    x: canvas.width / 2 - 40,
+    y: canvas.height - 20,
+    w: 80,
+    h: 10,
+    speed: 8,
+    dx: 0,
 }
-)
+
+
+// Draw paddle on canvas
+function drawPaddle() {
+    ctx.beginPath()
+    ctx.rect(paddle.x,paddle.y,paddle.w,paddle.h)
+    ctx.fillStyle = '#0095dd'
+    ctx.fill()
+    ctx.closePath()
+}
+
+
+// Draw score on canvas
+function drawScore() {
+    ctx.font = '20px Arial'
+    ctx.fillText(`Score: ${score}`, canvas.width-100, 30)
+}
+
+
+// Draw everything
+function draw() {
+    drawPaddle()
+    drawBall()
+    drawScore()
+}
+
+draw()
